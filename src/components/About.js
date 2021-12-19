596c3f8ab3fe277722fd8394b04c81b1d2de9900
Subhadipa <bsubhadipa@gmail.com>
2021-12-19 16:41:16 +0530

    Project ReactJs Done

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+const About = () => 
+{
+    return (
+        <div>
+        
+         <h4>Version 1.0.0</h4>
+         <h5>Subhadipa Banerjee</h5>
+         <a href='/'>Go Back</a>   
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p>Copyright &copy;2021</p>
+            <h5>Subhadipa Banerjee</h5>
+            <a href="/about">About</a>
+        </footer>
+    )
+}
+
+export default Footer
